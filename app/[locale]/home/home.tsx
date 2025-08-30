69457580ac4e7d2bc009952f38d00665c8ceea8c
mehediT <mehedi.toure@gmail.com>
2025-08-30 20:09:49 +0200

    Refactor Home component to use section element and improve layout structure

--- app/[locale]/home/home.tsx
+++ app/[locale]/home/home.tsx
@@ -18,9 +18,9 @@ export default async function Home(
   const config = await siteConfig();
   const t = await getScopedI18n('home')
     return (
-      <div
+      <section
         id={id}
-        className={className}
+        className={`${className} flex flex-col items-center justify-center gap-4`}
       >
         <div className="space-y-2 w-2/3">
           <p
@@ -69,14 +69,6 @@ export default async function Home(
             GitHub
           </Link>
         </div>
-
-        <div className="mt-8">
-          <Snippet hideCopyButton hideSymbol variant="bordered">
-            <span>
-              Get started by editing <Code color="primary">app/page.tsx</Code>
-            </span>
-          </Snippet>
-        </div>
-      </div>
+      </section>
     );
 }
